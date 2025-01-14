1d715d77588b45e368fb57bf9c850dcb650cc742
Jeancyjr <85275214+Jeancyjr@users.noreply.github.com>
2025-01-14 23:50:39 +0100

    interactive-rating-component-main
    interactive-rating-component-main

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const ratingNumber = document.getElementById("rating");
+const submitBtn = document.querySelector(".btn");
+const lightMode = document.getElementById("light-mode");
+
+
+
+lightMode.addEventListener("click", turnOnOff);
