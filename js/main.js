9055df308943cae215392f04d4dfc3d393b9ebf1
Jeancyjr <85275214+Jeancyjr@users.noreply.github.com>
2025-01-17 00:32:59 +0100

    interactive rating component main
    interactive rating component main

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-const ratingNumber = document.getElementById("rating");
-const submitBtn = document.querySelector(".btn");
-const lightMode = document.getElementById("light-mode");
+const ratingNumbers = document.querySelectorAll('.rating-number li');
+  const submitButton = document.querySelector('.btn');
+  const firstComponent = document.querySelector('#first-component');
+  const secondComponent = document.querySelector('#second-component');
+  const selectedRatingText = document.querySelector('#selected');
+  const switchMode = document.getElementById("light-mode");
 
+  let selectedRating;
 
+  ratingNumbers.forEach((number) => {
+    number.addEventListener('click', () => {
+      ratingNumbers.forEach((num) => num.classList.remove('selected'));
 
-lightMode.addEventListener("click", turnOnOff);
+     
+      number.classList.add('selected');
+      selectedRating = number.dataset.value;
+    });
+  });
+
+  submitButton.addEventListener('click', () => {
+    if (selectedRating) {
+      selectedRatingText.textContent = selectedRating;
+
+      firstComponent.setAttribute('aria-hidden', 'true');
+      firstComponent.style.display = 'none';
+      secondComponent.hidden = false;
+    } else {
+      alert('Please select a rating before submitting.');
+    }
+  });
+
+  switchMode.addEventListener("click", () => {
+    const switchBtn = document.body;
+    switchBtn.classList.toggle("light-mode");
+  });
